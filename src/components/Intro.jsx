b77a96e2e0de69c58eb021df6afef66dbc192b7e
AkivaKn <akivakaufman@gmail.com>
2024-09-21 15:45:17 +0100

    feat: initialise Vite React project with Tailwind
    Initialise project, set up Tailwind and create intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,26 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+export default function Intro() {
+  return (
+    <div className="h-[80vh] w-screen flex flex-col justify-center items-center">
+      <h1>Hi, I&apos;m Akiva</h1>
+      <h2>Junior Software Developer</h2>
+      <div className="flex">
+        <a href="https://github.com/AkivaKn" target="blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/akivakaufman/" target="blank">
+          <FaLinkedin />
+        </a>
+        <a href="mailto: akivakaufman@gmail.com" target="blank">
+          <MdEmail />
+        </a>
+      </div>
+      <p>
+        I have a passion for quality code, and enjoy constantly expanding my
+        skills.
+      </p>
+    </div>
+  );
+}
