61ddb23883521d86f16ea5c518571c91e8e1d89b
AkivaKn <akivakaufman@gmail.com>
2024-09-29 16:55:03 +0100

    feat: implement react router links in mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
+
 export default function MobileMenu({ setShowMobileMenu }) {
   const closeMenu = () => {
     setShowMobileMenu(false)
   }
     return (
       <nav className="flex flex-col">
-          <a href="#intro" onClick={closeMenu}>Home</a>
-          <a href="#about-me" onClick={closeMenu}>About me</a>
-          <a href="#projects" onClick={closeMenu}>Projects</a>
-          <a href="#contact" onClick={closeMenu}>Contact</a>
+          <Link to="/#intro" onClick={closeMenu}>Home</Link>
+          <Link to="/#about-me" onClick={closeMenu}>About me</Link>
+          <Link to="/#projects" onClick={closeMenu}>Projects</Link>
+          <Link to="/#contact" onClick={closeMenu}>Contact</Link>
         </nav>
     )
 }
